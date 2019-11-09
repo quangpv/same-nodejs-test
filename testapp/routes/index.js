04333d46e9ecadbd5bde51ab6d749caf6e6d8151
quangpv <quangpv.uit@gmail.com>
2019-11-09 13:26:30 +0700

    update

--- testapp/routes/index.js
+++ testapp/routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require("mysql");
 
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "billy",
+    password: "Abc12345"
+});
+var sqlState = -1;
+con.connect(function (err) {
+    sqlState = err ? 0 : 1
+});
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    if (sqlState === -1) {
+        res.send("Waiting for connection");
+        return;
+    }
+    if (sqlState === 0) {
+        res.send("Connect to database fail");
+        return;
+    }
+    con.query("select * from User", function (err, result) {
+        if (err) {
+            res.send("Connect to database fail");
+            return;
+        }
+        res.send(result);
+    });
 });
 
 module.exports = router;
